2921878a44694c2ff6cb4db31054c805b136262b
Danielle Arlantico <132239427+daniellearl@users.noreply.github.com>
2025-05-09 23:48:11 +1200

    route updates - finished

--- app/query/route.ts
+++ app/query/route.ts
@@ -3,13 +3,19 @@ import postgres from 'postgres';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
-	const data = await sql`
+  const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
   `;
-
-	return data;
+  return data;
 }
 
+export async function GET() {
+  try {
+    return Response.json(await listInvoices());
+  } catch (error) {
+    return Response.json({ error: String(error) }, { status: 500 });
+  }
+}
